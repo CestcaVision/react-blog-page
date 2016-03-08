908dca0515d23863c50e0bfd808f370e05588de7
Tricrepe <644568681@qq.com>
2016-03-09 00:02:45 +0800

    add AppLeftNav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React , { Component } from 'react';
 import Home from './Home';
-import Navbar from './shared/Navbar'
+import Navbar from './shared/Navbar';
 import AppBar from 'material-ui/lib/app-bar';
+import AppLeftNav from './shared/AppLeftNav';
 class App extends Component {
   constructor(props, context) {
     super(props,context);
@@ -16,18 +17,23 @@ class App extends Component {
     return (
       <div className='app-wrap'>
         {this.state.renderNavBar?<Navbar/>:this._getAppBar()}
+        <AppLeftNav ref="leftNav"/>
         {this.props.children}
         <div className='app-footer'></div>
       </div>
     )
   }
+  _onLeftIconButtonTouchTap(){
+    this.refs.leftNav.handleToggle()
+  }
   _getAppBar(){
     let title = this.context.router.isActive('/home')?'HOME':
     this.context.router.isActive('/about')?'About':
-    this.context.router.isActive('/contact')?'CONTACT':'';
-    return <AppBar title={title}/>
+    this.context.router.isActive('/contact')?'Contact':'';
+    return <AppBar title={title} onLeftIconButtonTouchTap={this._onLeftIconButtonTouchTap.bind(this)}/>
   }
 }
+
 App.contextTypes = {
   router: React.PropTypes.object.isRequired
 }
